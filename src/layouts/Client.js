d7a4ab620715b004913bce564f863eb6f7d9f1fd
Chandru0811 <chandru08112000@gmail.com>
2024-08-22 17:31:13 +0530

    first commit

--- src/layouts/Client.js
+++ src/layouts/Client.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/client/Home";
-
+import "../styles/client.css";
 function Client({
   handleLogout,
   handleLogin,
