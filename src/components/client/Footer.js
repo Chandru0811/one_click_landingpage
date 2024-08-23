6af4eb168919d28d915251dfa469b4a61fe412aa
Rohini0410 <rohinidevi.cloudecs@gmail.com>
2024-08-23 15:05:35 +0530

    footer completed

--- src/components/client/Footer.js
+++ src/components/client/Footer.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React from 'react';
+import { TbWorld } from "react-icons/tb";
+import { FaFacebookF, FaTwitter, FaGoogle, FaLinkedinIn, FaPhoneAlt } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { FaLocationArrow } from "react-icons/fa6";
+import { LiaCopyrightSolid } from "react-icons/lia";
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <section className='footerBg'>
+      <div className='container'>
+        <div className='row pt-5'>
+          <div className='col-md-4 col-12 mb-4'>
+            <h2 className='fw-bold'>ECS</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Diam ut id nisl tellus
+              rhoncus, imperdiet
+            </p>
+            <div className='d-flex'>
+              <span className='mediaIconBg'>
+                <TbWorld />
+              </span>
+              <span className='mediaIconBg mx-3'>
+                <FaFacebookF />
+              </span>
+              <span className='mediaIconBg'>
+                <FaTwitter />
+              </span>
+              <span className='mediaIconBg mx-3'>
+                <FaGoogle />
+              </span>
+              <span className='mediaIconBg'>
+                <FaLinkedinIn />
+              </span>
+            </div>
+          </div>
+          <div className='col-md-3 col-12 mb-3'>
+            <h5 className='mb-3 fw-bold'>Our Services</h5>
+            <p>One Click Social Media Posting</p>
+            <p>SEO Services</p>
+            <p>Social Media Marketing</p>
+            <p>CRM</p>
+          </div>
+          <div className='col-md-2 col-12 mb-3'>
+            <h5 className='mb-3 fw-bold'>Pages</h5>
+            <p>Contact Us</p>
+            <p>Register</p>
+            <p>Login</p>
+            <p>Book Demo</p>
+          </div>
+          <div className='col-md-3 col-12 mb-3'>
+            <h5 className='mb-3 fw-bold'>Contact</h5>
+            <div className='d-flex mb-3'>
+              <span className='me-2 mediaIconBg'><FaPhoneAlt /></span>
+              <h3 className='fw-bold'>+65 889 1306</h3>
+            </div>
+            <div className='d-flex mb-3'>
+              <span className='mediaIconBg1 me-2'>
+                <SiGmail />
+              </span>
+              <p>info@ecscloudinfotech.com</p>
+            </div>
+            <div className='d-flex'>
+              <span className='mediaIconBg1 me-2'>
+                <FaLocationArrow />
+              </span>
+              <p>The Alexcier, 237 Alexandra Road, #04-10</p>
+            </div>
+          </div>
+          <hr className='mb-0'/>
+          <div className='footer-copyright py-2'>
+            <LiaCopyrightSolid />
+            <p> 2024 | All Rights Reserved at <span className='footerText'>ECS Cloud Infotech</span></p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default Footer
+export default Footer;
